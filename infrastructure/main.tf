ab008f24f0beb8edf599506704a485adcc6b7f51
[Akhil Jose] <[akhiljos256@gmail.com]>
2025-03-06 21:07:55 -0500

    msg

---
+++ infrastructure/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+      }
+}
+
+resource "azurerm_resource_group"     "rg" {
+  name     = "my-resource-group"
+  location="East US"
+
+
+
+
+
+
+
+
+
+} 
